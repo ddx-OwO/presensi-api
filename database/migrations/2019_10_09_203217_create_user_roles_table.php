77eadc9dde80e1c0fee3f827ee5f704a91e39bf9
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-10-10 20:58:17 +0700

    update user roles table foreign key

--- database/migrations/2019_10_09_203217_create_user_roles_table.php
+++ database/migrations/2019_10_09_203217_create_user_roles_table.php
@@ -20,8 +20,14 @@ class CreateUserRolesTable extends Migration
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
 
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('role_id')->references('id')->on('roles');
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+            $table->foreign('role_id')
+                  ->references('id')
+                  ->on('roles')
+                  ->onDelete('cascade');
         });
     }
 
