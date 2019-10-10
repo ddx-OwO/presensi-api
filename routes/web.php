085378648cbe6ee0a200131969360bf2389f88af
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-10-11 00:52:26 +0700

    update router list

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->get('users', ['as' => 'users.index', 'uses' => 'UserController@index']);
+    $router->get('users/{id}', ['as' => 'users.show', 'uses' => 'UserController@show']);
+    $router->get('users/{id}/relationships/roles', ['as' => 'users.relationships.roles', 'uses' => 'UserController@roles']);
+    $router->post('users', ['as' => 'users.create', 'uses' => 'UserController@create']);
+    $router->post('users/{id}/change_password', ['as' => 'users.change_password', 'uses' => 'UserController@changePassword']);
+    $router->put('users/{id}', ['as' => 'users.update', 'uses' => 'UserController@update']);
+    $router->delete('users/{id}', ['as' => 'users.delete', 'uses' => 'UserController@destroy']);
+
+    $router->get('roles', ['as' => 'roles.index', 'uses' => 'RoleController@index']);
+    $router->get('roles/{name}', ['as' => 'roles.show', 'uses' => 'RoleController@show']);
+});
+
+$router->post('auth/login', 'AuthController@authenticate');
