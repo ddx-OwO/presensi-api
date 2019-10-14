01d2790d2e728bab73f2aa1903f319d6362f91dc
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-10-14 12:37:34 +0700

    membuat middleware peran

---
+++ app/Http/Middleware/StudentMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Helpers\JwtHelper;
+
+class StudentMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = explode(' ', $request->header('Authorization'));
+        $credentials = $request->credentials;
+
+        if (! in_array('siswa', $credentials->roles)) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Tidak memiliki akses untuk endpoint ini.'
+                ]
+            ], 403);
+        }
+        return $next($request);
+    }
+}
