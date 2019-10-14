794f266740e1d2549f58e5e27d0657d7cfb68902
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-10-14 12:50:22 +0700

    update user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Resources\User as UserResource;
-use App\Http\Resources\UserCollection;
 use App\Http\Resources\UserRoleRelationship;
 use App\User;
 
@@ -13,7 +12,7 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \App\Http\Resources\User
      */
     public function index(Request $request)
     {
@@ -27,9 +26,9 @@ class UserController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     * 
+     *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \App\Http\Resources\User
      */
     public function create(Request $request)
     {
@@ -50,7 +49,7 @@ class UserController extends Controller
         $user->address = $request->input('address');
         $user->dob = $request->input('date_of_birth') ? $request->input('date_of_birth') : date('2000-01-01');
         $user->save();
-        
+
         UserResource::withoutWrapping();
         return new UserResource($user);
     }
@@ -59,7 +58,7 @@ class UserController extends Controller
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \App\Http\Resources\User
      */
     public function show($id)
     {
@@ -78,27 +77,33 @@ class UserController extends Controller
     public function update(Request $request, $id)
     {
         $data = $request->all();
-        $password = password_hash($request->input('password'), PASSWORD_BCRYPT);
-
         $user = User::find($id);
         $user->fill($data);
-        $user->password = $request->input('password') ? $password : $user->password;
+        if ($request->input('password')) {
+            $user->password = password_hash($request->input('password'), PASSWORD_BCRYPT);;
+        }
+
         $user->save();
         UserResource::withoutWrapping();
         return new UserResource($user);
     }
 
+    /**
+     * Endpoint khusus untuk mengubah password
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function changePassword(Request $request, $id)
     {
         $validatedData = $this->validate($request, [
             'password' => 'required'
         ]);
-
-        $password = password_hash($request->input('password'), PASSWORD_BCRYPT);
-        $user = User::find($id);
-        $user->password = $password;
+        $user = User::findOrFail($id);
+        $user->password = $passwordpassword_hash($request->input('password'), PASSWORD_BCRYPT);
         $user->save();
-        return response()->json(['message' => 'Password berhasil diganti']);
+        return response()->json(['message' => 'Password berhasil diganti'], 200);
     }
 
     /**
@@ -112,9 +117,29 @@ class UserController extends Controller
         $user = User::find($id);
 
         $user->delete();
-        return response()->json([],204); 
+        return response()->json([], 204);
     }
 
+    /**
+     * Menampilkan data user sesuai credentials
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function showSelf(Request $request)
+    {
+        $credentials = $request->credentials;
+        $user = User::findOrFail($credentials->user_id);
+        UserResource::withoutWrapping();
+        return new UserResource($user);
+    }
+
+    /**
+     * Menampilkan daftar peran user
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
     public function roles($id)
     {
         $user = User::findOrFail($id);
